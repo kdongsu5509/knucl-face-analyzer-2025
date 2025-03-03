9c9f9a55023cdd7423802d8db1e8e0a67b5b5851
kdongsu5509 <kdongsu5509@gmail.com>
2025-03-04 00:00:54 +0900

    fix(이미지 업로드): Controller 설정 교정
    Controller 작성 시 애노테이션 실수가 있어 수정했습니다

--- src/main/java/com/knucl/FaceAnalyze/controller/ImgController.java
+++ src/main/java/com/knucl/FaceAnalyze/controller/ImgController.java
@@ -2,26 +2,34 @@ package com.knucl.FaceAnalyze.controller;
 
 import com.knucl.FaceAnalyze.myException.S3Exception;
 import com.knucl.FaceAnalyze.service.S3ImageService;
+import groovy.util.logging.Slf4j;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/img")
+@lombok.extern.slf4j.Slf4j
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/")
 public class ImgController {
 
-    @Autowired
     private S3ImageService imgService;
 
-    @PostMapping()
-    public String upload(MultipartFile img) throws S3Exception, IOException {
-        return imgService.upload(img);
+    @PostMapping("image")
+    public String upload(@RequestParam("img") MultipartFile img) throws S3Exception, IOException {
+        String s3Url = imgService.upload(img);
+        log.info("Image uploaded to: " + s3Url);
+        return s3Url;
     }
 
-    @DeleteMapping()
-    public String delete(String imgAddress) {
+    @DeleteMapping("image")
+    public String delete(@RequestParam("imgAddress") String imgAddress) {
         try {
             imgService.deleteImageFromS3(imgAddress);
             return "success";
