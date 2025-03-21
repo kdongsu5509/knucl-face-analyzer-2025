77e8338c1017922fd2132c7a3dae4cf8899971e3
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-03-21 09:44:52 +0900

    feat(결과 페이지 제공): 결과 페이지 위한 컨트롤러 및 html 추가

---
+++ src/main/java/com/knucl/FaceAnalyze/controller/ResultController.java
@@ -0,0 +1,30 @@
+package com.knucl.FaceAnalyze.controller;
+
+import com.knucl.FaceAnalyze.repository.ResultRepository;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@AllArgsConstructor
+public class ResultController {
+    private final ResultRepository resultRepository;
+
+    /**
+     * 사용자에게 결과 View 페이지를 제공합니다
+     *
+     * @param uuid
+     * @param model
+     * @return
+     */
+    @GetMapping("/face/result/{uuid}")
+    public String showResult(@PathVariable String uuid, Model model) {
+        String resultText = resultRepository.getResult(UUID.fromString(uuid));
+        model.addAttribute("resultText", resultText);
+        return "result";
+    }
+
+}
