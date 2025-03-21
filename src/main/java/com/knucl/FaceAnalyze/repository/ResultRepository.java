fca0931f5f30992fa98e58d9f8fd8a4565cd8f3a
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-03-21 09:25:56 +0900

    feat(결과 저장): 결과를 저장할 메모리 저장소 생성

---
+++ src/main/java/com/knucl/FaceAnalyze/repository/ResultRepository.java
@@ -0,0 +1,27 @@
+package com.knucl.FaceAnalyze.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ResultRepository {
+    static Map<UUID, String> result = new HashMap<>();
+
+    public static UUID saveResult(String result) {
+        UUID uuid = UUID.randomUUID();
+        ResultRepository.result.put(uuid, result);
+        return uuid;
+    }
+
+    public static String getResult(UUID uuid) {
+        return ResultRepository.result.get(uuid);
+    }
+
+    public static void deleteResult(UUID uuid) {
+        ResultRepository.result.remove(uuid);
+    }
+
+    public static void clearResults() {
+        ResultRepository.result.clear();
+    }
+}
